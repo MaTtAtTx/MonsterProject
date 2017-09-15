3345828a93c8a519453fcd9c57bc41e8288b8fd0
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-15 09:32:08 -0600

    Added interaction with the monster using scanner and the getters and setters.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -8,9 +9,30 @@ public class MonsterController
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
 		System.out.println(sample);
-		MarshmallowMonster realMonster = new MarshmallowMonster("Tom", 3, 4, 3.0, false);
+		MarshmallowMonster realMonster = new MarshmallowMonster("Tom", 2, 4, 3.0, false);
+		System.out.println(realMonster);
+		System.out.println("Matthew is hungry, so he is going to eat an eye.");
+		realMonster.setEyeCount(1);
 		System.out.println(realMonster);
 		
+		interactWithTheMonster(realMonster);
 	}
 
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to kow what to eat next.");
+		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount() + " arms.");
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		if (consumed > currentMonster.getArmCount())
+		{
+		System.out.println(currentMonster.getName() + " says they don't have that many arms.");
+		}
+		else
+		{
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
+		}
+	}
 }	
