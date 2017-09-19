0790b44c44ac3b908aa90189cfb5344af4e311f0
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-19 09:03:30 -0600

    Added an if statement to ask questions about tentacle amount.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -21,10 +21,11 @@ public class MonsterController
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{
 		System.out.println(currentMonster.getName() + " wants to kow what to eat next.");
-		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount() + " arms.");
+		System.out.println(currentMonster.getName() + " suggests you eat arms. They have " + currentMonster.getArmCount() + " arms.");
 		System.out.println("How many do you want to eat?");
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
+
 		if (consumed <= currentMonster.getArmCount() && consumed >= 0)
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
@@ -33,13 +34,35 @@ public class MonsterController
 		else
 		{
 			if (consumed > -1)
-			{	
+			{
 				System.out.println(currentMonster.getName() + " says they don't have that many arms.");
 			}
 			else
-			{	
+			{
 				System.out.println(currentMonster.getName() + " says that is an invalid amount.");
 			}
+		}
+
+		System.out.println(currentMonster.getName() + " suggest you eat tentacles next. They have " + currentMonster.getTentacleAmount() + " tentacles");
+		System.out.println("How many do you want to eat? (you can eat parts of tentacles too)");
+		double consumedTentacles = myScanner.nextDouble();
+
+		if (consumedTentacles > currentMonster.getTentacleAmount())
+		{
+			System.out.println(currentMonster.getName() + " says they don't have that many tentacles.");
+		}
+		else if (consumedTentacles < 0)
+		{
+			System.out.println(currentMonster.getName() + " says that is an invalid amount.");
+		}
+		else if (consumedTentacles == 0)
+		{
+			System.out.println(currentMonster.getName() + " thinks your not very hungry");
+		}
+		else
+		{
+			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumedTentacles);
+			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getTentacleAmount() + " arms left.");
 		}
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -8,13 +8,13 @@ public class MarshmallowMonster
 	private int armCount;
 	private double tentacleAmount;
 	private boolean hasBloop;
-	
+
 	public MarshmallowMonster()
 	{
-		//Unless we specify values, all data members
-		//are a zero, false, or null
+		// Unless we specify values, all data members
+		// are a zero, false, or null
 	}
-	
+
 	public MarshmallowMonster(String name, int eyeCount, int armCount, double tentacleAmount, boolean hasBloop)
 	{
 		this.name = name;
@@ -23,65 +23,65 @@ public class MarshmallowMonster
 		this.tentacleAmount = tentacleAmount;
 		this.hasBloop = hasBloop;
 	}
-	
-	//Getters
+
+	// Getters
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	public int getEyeCount()
 	{
 		return eyeCount;
 	}
-	
+
 	public int getArmCount()
 	{
 		return armCount;
 	}
-	
+
 	public double getTentacleAmount()
 	{
 		return tentacleAmount;
 	}
-	
+
 	public boolean hasBloop()
 	{
 		return hasBloop;
 	}
-	
-	//Setters
+
+	// Setters
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-	
+
 	public void setEyeCount(int eyeCount)
 	{
 		this.eyeCount = eyeCount;
 	}
-	
+
 	public void setArmCount(int armCount)
 	{
 		this.armCount = armCount;
 	}
-	
+
 	public void setTentacleAmount(double tentacleAmount)
 	{
 		this.tentacleAmount = tentacleAmount;
 	}
-	
+
 	public void setBloop(boolean hasBloop)
 	{
 		this.hasBloop = hasBloop;
 	}
-	
+
 	public String toString()
 	{
-		 String description = "Hi, I am a scary monster! Ny name is " + name + ", and I have " + eyeCount;
-		 description += " eyes and I have " + armCount + " arms, and I have " + tentacleAmount + " tentacles";
-		 description += " and my bloop existence is " + hasBloop + ".";
-		 
-		 return description;
+		String description = "Hi, I am a scary monster! Ny name is " + name + ", and I have " + eyeCount;
+		description += " eyes and I have " + armCount + " arms, and I have " + tentacleAmount + " tentacles";
+		description += " and my bloop existence is " + hasBloop + ".";
+
+		return description;
 	}
 }
