d691afa891961b9bcb683dcd8e28fe1652e13e78
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-21 08:46:44 -0600

    Added usage of popup class instead of System.out.println and created a helper metod for String to integer conversion.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -16,61 +16,81 @@ public class MonsterController
 	public void start()
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
-		System.out.println(sample);
+//		System.out.println(sample);
+		popup.displayText(sample.toString());
 		MarshmallowMonster realMonster = new MarshmallowMonster("Tom", 2, 4, 3.0, false);
-		System.out.println(realMonster);
-		System.out.println("Matthew is hungry, so he is going to eat an eye.");
+				
+//		System.out.println(realMonster);
+		popup.displayText(realMonster.toString());
+//		System.out.println("Matthew is hungry, so he is going to eat an eye.");
+		popup.displayText("Matthew is hungry, so he is going to eat an eye.");
 		realMonster.setEyeCount(1);
-		System.out.println(realMonster);
-
+//		System.out.println(realMonster);
+		popup.displayText(realMonster.toString());
 		interactWithTheMonster(realMonster);
 	}
 
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{
-		System.out.println(currentMonster.getName() + " wants to kow what to eat next.");
-		System.out.println(currentMonster.getName() + " suggests you eat arms. They have " + currentMonster.getArmCount() + " arms.");
-		System.out.println("How many do you want to eat?");
+//		System.out.println(currentMonster.getName() + " wants to know what to eat next.");
+		popup.displayText(currentMonster.getName() + " wants to know what to eat next.");
+//		System.out.println(currentMonster.getName() + " suggests you eat arms. They have " + currentMonster.getArmCount() + " arms.");
+		popup.displayText(currentMonster.getName() + " suggests you eat arms. They have " + currentMonster.getArmCount() + " arms.");
+//		System.out.println("How many do you want to eat?");
+		int specialAnswer;
+		String unconverted = popup.getResponse("How many do you want to eat?");
+		
+		specialAnswer = Integer.parseInt(unconverted);
+		
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
 
 		if (consumed <= currentMonster.getArmCount() && consumed >= 0)
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
+//			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
+			popup.displayText("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 		}
 		else
 		{
 			if (consumed > -1)
 			{
-				System.out.println(currentMonster.getName() + " says they don't have that many arms.");
+//				System.out.println(currentMonster.getName() + " says they don't have that many arms.");
+				popup.displayText(currentMonster.getName() + " says they don't have that many arms.");
 			}
 			else
 			{
-				System.out.println(currentMonster.getName() + " says that is an invalid amount.");
+//				System.out.println(currentMonster.getName() + " says that is an invalid amount.");
+				popup.displayText(currentMonster.getName() + " says that is an invalid amount.");
 			}
 		}
 
-		System.out.println(currentMonster.getName() + " suggest you eat tentacles next. They have " + currentMonster.getTentacleAmount() + " tentacles");
-		System.out.println("How many do you want to eat? (you can eat parts of tentacles too)");
+//		System.out.println(currentMonster.getName() + " suggest you eat tentacles next. They have " + currentMonster.getTentacleAmount() + " tentacles");
+		popup.displayText(currentMonster.getName() + " suggest you eat tentacles next. They have " + currentMonster.getTentacleAmount() + " tentacles");
+//		System.out.println("How many do you want to eat? (you can eat parts of tentacles too)");
+		popup.getResponse("How many do you want to eat? (you can eat parts of tentacles too)");
 		double consumedTentacles = myScanner.nextDouble();
 
 		if (consumedTentacles > currentMonster.getTentacleAmount())
 		{
-			System.out.println(currentMonster.getName() + " says they don't have that many tentacles.");
+//			System.out.println(currentMonster.getName() + " says they don't have that many tentacles.");
+			popup.displayText(currentMonster.getName() + " says they don't have that many tentacles.");
 		}
 		else if (consumedTentacles < 0)
 		{
-			System.out.println(currentMonster.getName() + " says that is an invalid amount.");
+//			System.out.println(currentMonster.getName() + " says that is an invalid amount.");
+			popup.displayText(currentMonster.getName() + " says that is an invalid amount.");
 		}
 		else if (consumedTentacles == 0)
 		{
-			System.out.println(currentMonster.getName() + " thinks your not very hungry");
+//			System.out.println(currentMonster.getName() + " thinks your not very hungry");
+			popup.displayText(currentMonster.getName() + " thinks your not very hungry");
 		}
 		else
 		{
 			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumedTentacles);
-			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getTentacleAmount() + " arms left.");
+//			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getTentacleAmount() + " arms left.");
+			popup.displayText("Ok, now " + currentMonster.getName() + " has " + currentMonster.getTentacleAmount() + " arms left.");
 		}
 		
 		popup.displayText("Hey look at me! :)");
@@ -78,4 +98,23 @@ public class MonsterController
 		System.out.println(answer);
 		popup.displayText(answer);
 	}
+
+	//Helper methods
+	private boolean isValidInteger(String sample)
+	{
+		boolean valid = false;
+				
+		try
+		{
+			Integer.parseInt(sample);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			popup.displayText("Only integer values are valid: " + sample + " is not.");
+		}
+		
+		return valid;
+	}
 }
+
