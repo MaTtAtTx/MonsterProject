b8eb86305a0082b38d86ad6ec2b98e22439b09c0
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-28 07:40:07 -0600

    Removed the commented system.out.println lines because project was integrated to using popups instead.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -18,53 +18,41 @@ public class MonsterController
 	{
 		boolean finished = true;
 		int count = 0;
-//		while(count < 100)
-//		{
-//			popup.displayText("I am so neat! My number is " + count + ".");
-//			count++;
-//		}
+		// while(count < 100)
+		// {
+		// popup.displayText("I am so neat! My number is " + count + ".");
+		// count++;
+		// }
 
-//		for (int loop = 0; loop < 15; loop += 1)
-//		{
-//			popup.displayText("I am looping " + (loop + 1) + " times out of 15");
-//		}
+		// for (int loop = 0; loop < 15; loop += 1)
+		// {
+		// popup.displayText("I am looping " + (loop + 1) + " times out of 15");
+		// }
 
 		MarshmallowMonster sample = new MarshmallowMonster();
-		// System.out.println(sample);
 		popup.displayText(sample.toString());
 		MarshmallowMonster realMonster = new MarshmallowMonster("Tom", 2, 4, 3.0, false);
 
-		// System.out.println(realMonster);
 		popup.displayText(realMonster.toString());
-		// System.out.println("Matthew is hungry, so he is going to eat an eye.");
 		popup.displayText("Matthew is hungry, so he is going to eat an eye.");
 		realMonster.setEyeCount(1);
-		// System.out.println(realMonster);
 		popup.displayText(realMonster.toString());
 		interactWithTheMonster(realMonster);
 	}
 
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{
-		// System.out.println(currentMonster.getName() + " wants to know what to eat next.");
 		popup.displayText(currentMonster.getName() + " wants to know what to eat next.");
-		// System.out.println(currentMonster.getName() + " suggests you eat arms. They have " +
-		// currentMonster.getArmCount() + " arms.");
 		popup.displayText(currentMonster.getName() + " suggests you eat arms. They have " + currentMonster.getArmCount() + " arms.");
-		// System.out.println("How many do you want to eat?");
-		
+
 		int specialInt = 0;
 		String unconverted = popup.getResponse("How many do you want to eat?");
-		while(!isValidInteger(unconverted))
+		while (!isValidInteger(unconverted))
 		{
 			popup.displayText("Try again!");
 			unconverted = popup.getResponse("How many arms??");
 		}
-		
-//		if (isValidInteger(unconverted))
-//		{
 		specialInt = Integer.parseInt(unconverted);
-//		}
 
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = 0;
@@ -73,58 +61,44 @@ public class MonsterController
 		if (consumed <= currentMonster.getArmCount() && consumed >= 0)
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			// System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount()
-			// + " arms left.");
 			popup.displayText("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 		}
 		else
 		{
 			if (consumed > -1)
 			{
-				// System.out.println(currentMonster.getName() + " says they don't have that many arms.");
 				popup.displayText(currentMonster.getName() + " says they don't have that many arms.");
 			}
 			else
 			{
-				// System.out.println(currentMonster.getName() + " says that is an invalid amount.");
 				popup.displayText(currentMonster.getName() + " says that is an invalid amount.");
 			}
 		}
 
-		// System.out.println(currentMonster.getName() + " suggest you eat tentacles next. They have " +
-		// currentMonster.getTentacleAmount() + " tentacles");
 		popup.displayText(currentMonster.getName() + " suggest you eat tentacles next. They have " + currentMonster.getTentacleAmount() + " tentacles");
-		// System.out.println("How many do you want to eat? (you can eat parts of tentacles too)");
-		
+
 		String unconverted2 = popup.getResponse("How many do you want to eat? (you can eat parts of tentacles too)");
-		while(!isValidDouble(unconverted2))
+		while (!isValidDouble(unconverted2))
 		{
 			unconverted2 = popup.getResponse("You have to type a double! How many tentacles do you want to eat??");
 		}
 		double consumedTentacles = Double.parseDouble(unconverted2);
-		
-//		double consumedTentacles = myScanner.nextDouble();
 
 		if (consumedTentacles > currentMonster.getTentacleAmount())
 		{
-			// System.out.println(currentMonster.getName() + " says they don't have that many tentacles.");
 			popup.displayText(currentMonster.getName() + " says they don't have that many tentacles.");
 		}
 		else if (consumedTentacles < 0)
 		{
-			// System.out.println(currentMonster.getName() + " says that is an invalid amount.");
 			popup.displayText(currentMonster.getName() + " says that is an invalid amount.");
 		}
 		else if (consumedTentacles == 0)
 		{
-			// System.out.println(currentMonster.getName() + " thinks your not very hungry");
 			popup.displayText(currentMonster.getName() + " thinks your not very hungry");
 		}
 		else
 		{
 			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumedTentacles);
-			// System.out.println("Ok, now " + currentMonster.getName() + " has " +
-			// currentMonster.getTentacleAmount() + " arms left.");
 			popup.displayText("Ok, now " + currentMonster.getName() + " has " + currentMonster.getTentacleAmount() + " arms left.");
 		}
 
